638a74eaf2e9bd80597ed759b645b8f070cecd03
wenchao <860694896@qq.com>
2019-08-16 06:47:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/superrecyclerview/FeedAdapter.java
@@ -0,0 +1,86 @@
+package com.example.superrecyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.FeedHolder> {
+
+    @NonNull
+    @Override
+    public FeedHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_feed, parent, false);
+        return new FeedHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FeedHolder holder, int position) {
+        Picasso.with(holder.itemView.getContext())
+                .load(getAvatarResId(position))
+                .fit()
+                .into(holder.mIvAvatar);
+        Picasso.with(holder.itemView.getContext())
+                .load(getContentResId(position))
+                .fit()
+                .into(holder.mIvContent);
+        holder.mTvNickname.setText("Nickname " + position);
+    }
+
+    private int getAvatarResId(int position) {
+        switch (position % 4) {
+            case 0:
+                return R.drawable.avatar1;
+            case 1:
+                return R.drawable.avatar2;
+            case 2:
+                return R.drawable.avatar3;
+            case 3:
+                return R.drawable.avatar4;
+            default:
+                break;
+        }
+        return 0;
+    }
+
+    private int getContentResId(int position) {
+        switch (position % 4) {
+            case 0:
+                return R.drawable.taeyeon_one;
+            case 1:
+                return R.drawable.taeyeon_two;
+            case 2:
+                return R.drawable.taeyeon_three;
+            case 3:
+                return R.drawable.taeyeon_four;
+            default:
+                break;
+        }
+        return 0;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 100;
+    }
+
+    public static class FeedHolder extends RecyclerView.ViewHolder {
+
+        ImageView mIvAvatar;
+        ImageView mIvContent;
+        TextView mTvNickname;
+
+        public FeedHolder(@NonNull View itemView) {
+            super(itemView);
+            mIvAvatar = itemView.findViewById(R.id.iv_avatar);
+            mIvContent = itemView.findViewById(R.id.iv_content);
+            mTvNickname = itemView.findViewById(R.id.tv_nickname);
+        }
+    }
+}
